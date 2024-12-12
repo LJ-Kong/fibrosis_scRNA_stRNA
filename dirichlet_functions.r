c757586925f4a68d2e5766e627459f96861d38be
LJ-Kong <115665001+LJ-Kong@users.noreply.github.com>
2024-12-12 15:02:32 -0500

    dirichlet_functions.r

--- dirichlet_functions.r
+++ dirichlet_functions.r
@@ -1,8 +1,3 @@
-# Use dirichlet-multinomial regression to find significant changes in cell frequencies during disease
-# Fuctions were modified based on Smillie C.S. scripts at: 
-# https://github.com/cssmillie/ulcerative_colitis/blob/master/analysis.r
-# ---------------------------------------------------------------------------------------------------
-
 library(lmerTest)
 library(MASS)
 library(egg)
